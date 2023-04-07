33e7845b29b220f4a1f31e95d0658876377df324
Paul Villanueva <pvillanueva13@gmail.com>
2023-04-06 21:40:29 -0500

    Updates

--- src/setup.R
+++ src/setup.R
@@ -4,3 +4,6 @@ library(shinyWidgets)
 library(shiny)
 
 theme_set(theme_light())
+
+ensemble_model <- readRDS("src/models/ensemble_model_prio_roc_auc.rds")
+workflow_set <- readRDS("src/models/hab_models_1_200.rds")
